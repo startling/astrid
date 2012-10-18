3d4430a384d4e0c57e7b6d0163ec289a92a5d230
startling <tdixon51793@gmail.com>
2012-10-18 06:20:44 -0400

    a main module that exports everything.

---
+++ Phial.hs
@@ -0,0 +1,9 @@
+module Phial
+  ( module Phial.Rendering
+  , module Phial.Tile
+  , module Phial.Planes
+  , module Phial.Stages ) where
+import Phial.Rendering
+import Phial.Tile
+import Phial.Planes
+import Phial.Stages
