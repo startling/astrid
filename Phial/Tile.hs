6ebd1cd653d27096023442a5f1874f7ae90a19d3
startling <tdixon51793@gmail.com>
2012-10-18 03:23:36 -0400

    Turn a plane of tiles into the visible plane. I think.

--- Phial/Tile.hs
+++ Phial/Tile.hs
@@ -1,13 +1,28 @@
 module Phial.Tile where
+import Phial.Planes
 
 -- | Things that know how to move themselves.
 data Monster = Heroine
+  deriving (Show)
 
 -- | Things that never move.
 data Fixture = Wall
+  deriving (Eq, Show)
 
 -- | A space that has some monsters and fixtures.
 data Tile = Tile
   { monsters :: [Monster]
   , fixtures :: [Fixture] }
-  deriving (Eq, Show)
+  deriving (Show)
+
+-- | Tell whether this tile blocks the tiles behind it.
+blocks :: Tile -> Bool
+blocks = elem Wall . fixtures
+
+-- | Turn an infinite plane into a (hopefully finite) plane
+-- of visible tiles.
+view :: Plane Tile -> Plane Tile
+view (Plane s) = Plane . fmap viewNS . viewEW $ s
+  where
+    viewNS = both (takeWhile (not . blocks))
+    viewEW = both (takeWhile (not . (blocks . during)))
