b9b8530febba2ab2ab2bcb0b39888990c8267c63
startling <tdixon51793@gmail.com>
2012-10-19 14:32:25 -0400

    Phial exports Phial.Movement.

--- Phial.hs
+++ Phial.hs
@@ -2,8 +2,10 @@ module Phial
   ( module Phial.Rendering
   , module Phial.Tile
   , module Phial.Planes
+  , module Phial.Movement
   , module Phial.Stages ) where
 import Phial.Rendering
 import Phial.Tile
 import Phial.Planes
+import Phial.Movement
 import Phial.Stages
