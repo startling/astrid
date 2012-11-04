d6d55f51b103b1dbed539975689e72e2501aa00e
startling <tdixon51793@gmail.com>
2012-11-04 17:49:22 -0500

    `shifted', an isomorphism version of `shift'.

--- Astrid/Space.hs
+++ Astrid/Space.hs
@@ -1,6 +1,8 @@
 {-# Language MultiParamTypeClasses #-}
 {-# Language FunctionalDependencies #-}
 module Astrid.Space where
+-- lens:
+import Control.Lens
 
 class Direction d where
   -- | A list of directions.
@@ -8,9 +10,16 @@ class Direction d where
   -- | Invert a given direction.
   inverse :: d -> d
 
+-- | A space that we can move around in. Minimal complete definition
+-- is 'focus' and either 'shift' or 'shifted'.
 class Direction d => Space w d | w -> d where
   -- | Traverse this space shifted one unit in any direction.
   shift :: Functor f => d -> (w a -> f (w a)) -> w a -> f (w a)
+  shift = shifted
+  -- | An isomorphism to this space shifted one unit in some direction.
+  shifted :: (Functor f, Isomorphic k) => d
+    -> k (w a -> f (w a)) (w a -> f (w a))
+  shifted d = isomorphic (shift d) (shift $ inverse d)
   -- | Traverse the origin tile of this space.
   focus :: Functor f => (a -> f a) -> w a -> f (w a)
 
