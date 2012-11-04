f86dea6744e31a5e9a61ac14fc3ce78fe1199ebe
startling <tdixon51793@gmail.com>
2012-11-04 15:56:50 -0500

    rewrote `shift` and `focus` as traversals, got rid of lens import.

--- Astrid/Space.hs
+++ Astrid/Space.hs
@@ -1,8 +1,6 @@
 {-# Language MultiParamTypeClasses #-}
 {-# Language FunctionalDependencies #-}
 module Astrid.Space where
--- lens:
-import Control.Lens
 
 class Direction d where
   -- | A list of directions.
@@ -11,11 +9,11 @@ class Direction d where
   inverse :: d -> d
 
 class Direction d => Space w d | w -> d where
-  -- | A lens on the worlds shifted from this one.
-  shift :: d -> Simple Lens (w a) (w a)
-  -- | The focused tile of this world.
-  focus :: Simple Lens (w a) a
+  -- | Traverse this space shifted one unit in any direction.
+  shift :: Functor f => d -> (w a -> f (w a)) -> w a -> f (w a)
+  -- | Traverse the origin tile of this space.
+  focus :: Functor f => (a -> f a) -> w a -> f (w a)
 
--- | A lens on a neighboring tile.
-one :: (Space w d) => d -> Simple Lens (w a) a
+-- | Traverse a neighboring tile of the origin in some space.
+one :: (Space w d, Functor f) => d -> (a -> f a) -> w a -> f (w a)
 one x = shift x . focus
