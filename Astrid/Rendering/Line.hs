bc679096524cbdc42c9ae7897de434e5bd96ef9f
startling <tdixon51793@gmail.com>
2012-11-06 00:34:58 -0500

    Render lines.

---
+++ Astrid/Rendering/Line.hs
@@ -0,0 +1,21 @@
+module Astrid.Rendering.Line where
+-- astrid:
+import Astrid.Space.Line
+import Astrid.Objects
+-- curslet:
+import UI.Curslet
+
+renderLine :: Curslet m w => Line Tile -> m ()
+renderLine line = do
+  let (Line b d a) = fmap seeTile line
+  -- Center the cursor.
+  (y, x) <- center
+  -- Find out how much room we have on the right.
+  (_, m) <- getmax
+  let length = fromIntegral $ m - x
+  -- Put the center and the things to the right.
+  put . (d :) . take length $ a
+  -- Move to the center-left.
+  move (y, 0)
+  -- Figure out what's on the left side.
+  put . reverse . take (fromIntegral x) $ b
