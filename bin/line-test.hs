5ce4267200e50ea844408c4cfd4a023d62ed96f5
startling <tdixon51793@gmail.com>
2012-11-06 00:35:16 -0500

    small executable that renders a mostly-empty world.

---
+++ bin/line-test.hs
@@ -0,0 +1,17 @@
+-- astrid:
+import Astrid.Objects
+import Astrid.Space
+import Astrid.Space.Line
+import Astrid.Rendering.Line
+-- curslet:
+import UI.Curslet
+import UI.Curslet.Ncurses
+-- lens:
+import Control.Lens
+
+main :: IO ()
+main = runNcurses $ do
+  -- Make a world empty but for our Heroine.
+  let line = focus %~ (:) Heroine $ only empty
+  refresh $ renderLine line
+  getch >> return ()
