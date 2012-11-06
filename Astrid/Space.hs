a5ebf247a6616aaf6aaa59fbabc5eca4b4ce5ce3
startling <tdixon51793@gmail.com>
2012-11-06 02:14:15 -0500

    changed the definition of Space; shift is just a function.
    this should make the ismorphism behave as expected.

--- Astrid/Space.hs
+++ Astrid/Space.hs
@@ -13,16 +13,15 @@ class Direction d where
 -- | A space that we can move around in. Minimal complete definition
 -- is 'focus' and either 'shift' or 'shifted'.
 class Direction d => Space w d | w -> d where
-  -- | Traverse this space shifted one unit in any direction.
-  shift :: Functor f => d -> (w a -> f (w a)) -> w a -> f (w a)
-  shift = shifted
+  -- | Move the focus one cell in some direction.
+  shift :: d -> w a -> w a
   -- | An isomorphism to this space shifted one unit in some direction.
   shifted :: (Functor f, Isomorphic k) => d
     -> k (w a -> f (w a)) (w a -> f (w a))
-  shifted d = isomorphic (shift d) (shift $ inverse d)
+  shifted d = iso (shift d) (shift $ inverse d)
   -- | Traverse the origin tile of this space.
   focus :: Functor f => (a -> f a) -> w a -> f (w a)
 
 -- | Traverse a neighboring tile of the origin in some space.
 one :: (Space w d, Functor f) => d -> (a -> f a) -> w a -> f (w a)
-one x = shift x . focus
+one x = shifted x . focus
